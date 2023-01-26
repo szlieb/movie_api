da3cf41cd47274b64bfad30ed9f7f5cd76bb47a1
Zalmy L <shneurlieberman@gmail.com>
2023-01-26 13:19:24 -0500

    Added auth .js

---
+++ auth.js
@@ -0,0 +1,37 @@
+const jwtSecret = "your_jwt_secret";
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+require("./passport");
+
+// web token configureration
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username,
+    expiresIn: "7d",
+    algorithm: "HS256"
+  });
+};
+
+
+
+//POST on login authentication
+module.exports = (router) => {
+  router.post("/login", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
+      if (error || !user) {
+        console.error(error);
+        return res.status(400).json({
+          message: "Username or Password Invalid.",
+          user: user,
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+};
